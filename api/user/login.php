e734cbd1931a23817755aa457d37fb19b1f30bf1
Parth-Chavda <parth18chavda@gmail.com>
2025-08-02 14:55:58 +0530

    api updated

--- api/user/login.php
+++ api/user/login.php
@@ -6,53 +6,64 @@ require_once "../../includes/init.php";
 
 $Username = $_POST['Username'] ?? null;
 $Password = $_POST['Password'] ?? null;
-$userType = $_POST['user_type'] ?? null;
 
-if (!$userType) {
-    echo json_encode(['success' => false, 'reason' => 'NoUserType']);
-    exit;
+function trylogin($conn,$table,$Username,$Password){
+    $q = "SELECT * FROM `$table` WHERE `Username` = ? AND `Password` = ?";
+    $param = [$Username,$Password];
+
+    $stmt = $conn->prepare($q);
+    $stmt->execute($param);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
-if ($userType === 'admin') {
-    $table = "User";
-} elseif ($userType === 'student') {
-    $table = "student";
-} else {
-    echo json_encode(['success' => false, 'reason' => 'InvalidType']);
-    exit;
-}
-
-$q = "SELECT * FROM `$table` WHERE `Username` = ? and `Password` = ? ";
-$param = [$Username,$Password];
-
-$stmt = $conn->prepare($q);
-$stmt->execute($param);
-
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+$user = trylogin($conn,"User",$Username,$Password);
 if($user){
     $_SESSION['loggedIn'] = true;
     $_SESSION['user'] = $user['id'];
     $_SESSION['email'] = $user['Email'];
-    $_SESSION['role'] = $userType;
+    $_SESSION['role'] = $user['role'];
     $_SESSION['Username'] = $user['Username'];
-    if($userType == 'student'){
-        $_SESSION['class'] = $user['Class']; 
-        $_SESSION['name'] = $user['Username'];
-    }
-    echo json_encode(['success' => true, 'user' => $user ,'session_after_set' => $_SESSION ]);
+    echo json_encode(['success' => true, 'user' => $user]);
     exit;
+}
+$user = trylogin($conn,"student",$Username,$Password);
+if($user){
+    $_SESSION['loggedIn'] = true;
+    $_SESSION['user'] = $user['id'];
+    $_SESSION['email'] = $user['Email'];
+    $_SESSION['role'] = $user['role'];
+    $_SESSION['Username'] = $user['Username'];
+    $_SESSION['class'] = $user['Class'];
+    $_SESSION['name'] = $user['Username'];
+
+    echo json_encode(['success' => true, 'user' => $user]);
+    exit;
+}
+
+$foundUsername = false;
+
+$q2 = "SELECT Username FROM `User` WHERE `Username` = ?";
+$param = [$Username];
+
+$stmt = $conn->prepare($q2);
+$stmt->execute($param);
+
+if($stmt->fetch()){
+    $foundUsername = true;
+}
+
+$q3 = "SELECT Username FROM `student` WHERE `Username` = ?";
+$param = [$Username];
+$stmt = $conn->prepare($q3);
+$stmt->execute($param);
+
+if($stmt->fetch()){
+    $foundUsername = true;
+}
+
+if($foundUsername){
+    echo json_encode(['success' => false , 'reason' => 'Password']);
 }else{
+    echo json_encode(['success' => false , 'reason' => 'Username']);
 
-    $q2 = "SELECT * FROM `$table` WHERE `Username` = ?";
-
-    $stmt2= $conn->prepare($q2);
-    $stmt2->execute([$Username]);
-    $checkusername = $stmt2->fetch(PDO::FETCH_ASSOC);
-
-    if (!$checkusername) {
-        echo json_encode(['success' => false, 'reason' => 'Username']);
-    } else {
-        echo json_encode(['success' => false, 'reason' => 'Password']);
-    }
 }
